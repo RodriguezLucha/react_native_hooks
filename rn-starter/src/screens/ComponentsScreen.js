3f20dcea6e9490378fb97289fde3c4112125771d
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2020-01-01 17:49:09 -0800

    fixing repository

---
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const ComponentsScreen = () => {
+  const theMessage = "jumps over the lazy dog!";
+
+  return (
+    <View>
+      <Text style={styles.textStyle}>The quick brown fox</Text>
+      <Text style={styles.secondTextStyle}>{theMessage}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20
+  },
+  secondTextStyle: {
+    fontSize: 25,
+    fontFamily: "Courier New"
+  }
+});
+
+export default ComponentsScreen;
