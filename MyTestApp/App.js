a8b27f1772ac2c5025b89c23cd42474c88ec2268
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2020-05-03 09:14:44 -0700

    Moving the circle around

--- MyTestApp/App.js
+++ MyTestApp/App.js
@@ -7,17 +7,35 @@ import {
   Text,
   StatusBar,
   TouchableOpacity,
+  Animated,
 } from 'react-native';
 
 import {
   Header,
   Colors,
   ReloadInstructions,
+  State,
 } from 'react-native/Libraries/NewAppScreen';
+
+import {PanGestureHandler} from 'react-native-gesture-handler';
 
 import {useSpring, animated} from 'react-spring';
 
 const AnimatedView = animated(TouchableOpacity);
+
+let translateX = new Animated.Value(0);
+let translateY = new Animated.Value(0);
+let handleGesture = Animated.event(
+  [
+    {
+      nativeEvent: {
+        translationX: translateX,
+        translationY: translateY,
+      },
+    },
+  ],
+  {useNativeDriver: true},
+);
 
 const App: () => React$Node = () => {
   const [state, toggle] = useState(true);
@@ -40,6 +58,17 @@ const App: () => React$Node = () => {
 
   const onPress = () => toggle(!state);
 
+  let circleTransformStyle = {
+    transform: [
+      {
+        translateY: translateY,
+      },
+      {
+        translateX: translateX,
+      },
+    ],
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -60,7 +89,15 @@ const App: () => React$Node = () => {
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
               </Text>
+
               <AnimatedView style={circleProps} onPress={onPress} />
+              <View style={[styles.container]}>
+                <PanGestureHandler onGestureEvent={handleGesture}>
+                  <Animated.View
+                    style={[styles.circle, circleTransformStyle]}
+                  />
+                </PanGestureHandler>
+              </View>
             </View>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>See Your Changes</Text>
@@ -96,6 +133,26 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  circle: {
+    width: 150,
+    height: 150,
+    backgroundColor: '#c00000',
+    borderRadius: 100,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+  },
+  box: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center',
+    backgroundColor: 'plum',
+    margin: 10,
+    zIndex: 200,
   },
 });
 
