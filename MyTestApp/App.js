e70f0926fe9789b258b717481861f85dce0d0f8e
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2020-05-03 07:52:45 -0700

    Delete some unused stuff

--- MyTestApp/App.js
+++ MyTestApp/App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {useState} from 'react';
 import {
   SafeAreaView,
@@ -13,21 +5,17 @@ import {
   ScrollView,
   View,
   Text,
-  Donut,
   StatusBar,
-  TouchableWithoutFeedback,
   TouchableOpacity,
 } from 'react-native';
 
 import {
   Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import {Spring, useSpring, animated} from 'react-spring';
+import {useSpring, animated} from 'react-spring';
 
 const AnimatedView = animated(TouchableOpacity);
 
@@ -35,8 +23,8 @@ const App: () => React$Node = () => {
   const [state, toggle] = useState(true);
 
   let circleProps = useSpring({
-    width: state ? 200 : 50,
-    height: state ? 200 : 50,
+    width: state ? 200 : 100,
+    height: state ? 200 : 100,
     backgroundColor: state ? 'green' : 'blue',
     borderRadius: state ? 70 : 100,
     config: {
@@ -44,8 +32,8 @@ const App: () => React$Node = () => {
     },
     from: {
       backgroundColor: 'green',
-      width: 50,
-      height: 50,
+      width: 100,
+      height: 100,
       mass: 250,
     },
   });
@@ -67,7 +55,7 @@ const App: () => React$Node = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One!!</Text>
+              <Text style={styles.sectionTitle}>Demo app</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
@@ -80,19 +68,6 @@ const App: () => React$Node = () => {
                 <ReloadInstructions />
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -101,13 +76,6 @@ const App: () => React$Node = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
     backgroundColor: Colors.white,
   },
@@ -128,14 +96,6 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
